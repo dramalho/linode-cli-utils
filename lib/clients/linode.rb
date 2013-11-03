19edea9ac475e288b31a02d93c1eb88417326a26
David Ramalho <dramalho@gmail.com>
2013-11-03 17:40:37 +0000

    Removed config files

--- lib/clients/linode.rb
+++ lib/clients/linode.rb
@@ -1,41 +1,41 @@
-require 'linode'
+module LinodeCliUtils
+  module Clients
+    class Linode
+      def initialize(options = {})
+        if options.include?(:config_file)
+          load_configuration_from_file(options[:config_file])
+        else
+          configure(options)
+        end
 
-module Clients
-  class Linode
-    def initialize(options = {})
-      if options.include?(:config_file)
-        load_configuration_from_file(options[:config_file])
-      else
-        configure(options)
+        @client = ::Linode.new(api_key: @api_key)
       end
 
-      @client = ::Linode.new(api_key: @api_key)
-    end
-
-    def list_boxes
-      boxes = @client.linode.list
-      boxes.map do |box|
-        # Find the IP's
-        ips = @client.linode.ip.list(LinodeId: box.linodeid)
-        {
-          label: box.label, 
-          ip: ips.detect {|ip| ip.ispublic == 1}.ipaddress
-        }
+      def list_boxes
+        boxes = @client.linode.list
+        boxes.map do |box|
+          # Find the IP's
+          ips = @client.linode.ip.list(LinodeId: box.linodeid)
+          {
+            label: box.label, 
+            ip: ips.detect {|ip| ip.ispublic == 1}.ipaddress
+          }
+        end
       end
-    end
 
-    protected
+      protected
 
-    def load_configuration_from_file(file)
-      cfg = YAML.load_file(file)
+      def load_configuration_from_file(file)
+        cfg = YAML.load_file(file)
 
-      raise "Invalid configuration file #{file}" unless cfg
+        raise "Invalid configuration file #{file}" unless cfg
 
-      configure(cfg.to_hash)
-    end
+        configure(cfg.to_hash)
+      end
 
-    def configure(options = {})
-      @api_key = options['api']['key']
+      def configure(options = {})
+        @api_key = options['api']['key']
+      end
     end
   end
 end
--- lib/config/linode-cli-utils.rb
+++
@@ -1,3 +0,0 @@
-APP_ROOT = File.expand_path(File.dirname(__FILE__) + '/..') unless defined?(APP_ROOT)
-
-Dir[File.join( APP_ROOT, 'lib', '**', "*.rb" )].each {|file| require file}
